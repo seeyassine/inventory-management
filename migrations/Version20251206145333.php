6960e9519300fa556f195f78b30b74d61d49b08c
See Yassine <seeyassine@gmail.com>
2025-12-12 23:03:29 +0100

    change megrations

--- migrations/Version20251206145333.php
+++ migrations/Version20251206145333.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20251118225422 extends AbstractMigration
+final class Version20251206145333 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,15 +21,18 @@ final class Version20251118225422 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE client (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, adresse LONGTEXT NOT NULL, email VARCHAR(180) NOT NULL, telephone VARCHAR(20) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE forecast (id INT AUTO_INCREMENT NOT NULL, location_id INT NOT NULL, date DATE NOT NULL, celsius INT NOT NULL, INDEX IDX_2A9C784464D218E (location_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE location (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, country_code VARCHAR(2) NOT NULL, latitude NUMERIC(10, 7) NOT NULL, longitude NUMERIC(10, 7) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql("INSERT INTO location(name, country_code, latitude, longitude) VALUES('Barcelona', 'ES', 41.38879, 2.15899)");
+        $this->addSql('ALTER TABLE forecast ADD CONSTRAINT FK_2A9C784464D218E FOREIGN KEY (location_id) REFERENCES location (id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE forecast DROP FOREIGN KEY FK_2A9C784464D218E');
         $this->addSql('DROP TABLE client');
+        $this->addSql('DROP TABLE forecast');
         $this->addSql('DROP TABLE location');
         $this->addSql('DROP TABLE `user`');
     }
