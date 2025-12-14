9c83b421f09bddaa30eec23d57a47399c05e71e6
See Yassine <seeyassine@gmail.com>
2025-12-15 00:41:46 +0100

    Assuming BaseEntity provides common fields like createdAt, updatedAt

--- src/Entity/MouvementStock.php
+++ src/Entity/MouvementStock.php
@@ -6,7 +6,7 @@ use App\Repository\MouvementStockRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: MouvementStockRepository::class)]
-class MouvementStock
+class MouvementStock extends BaseEntity
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
