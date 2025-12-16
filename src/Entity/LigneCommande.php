1b62cbbba039c7bcafa4369ab513c67c63c9b6e1
See Yassine <seeyassine@gmail.com>
2025-12-16 22:32:05 +0100

    add entity lingeCommande

---
+++ src/Entity/LigneCommande.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Entity;
+
+use App\Enum\StatutCommande;
+use App\Repository\LigneCommandeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LigneCommandeRepository::class)]
+class LigneCommande extends BaseEntity
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $quantite = null;
+
+    #[ORM\Column]
+    private ?float $prixAchat = null;
+
+    #[ORM\Column]
+    private ?float $sousTotal = null;
+
+    #[ORM\Column(enumType: StatutCommande::class)]
+    private ?StatutCommande $statut;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getQuantite(): ?int
+    {
+        return $this->quantite;
+    }
+
+    public function setQuantite(int $quantite): static
+    {
+        $this->quantite = $quantite;
+
+        return $this;
+    }
+
+    public function getPrixAchat(): ?float
+    {
+        return $this->prixAchat;
+    }
+
+    public function setPrixAchat(float $prixAchat): static
+    {
+        $this->prixAchat = $prixAchat;
+
+        return $this;
+    }
+
+    public function getSousTotal(): ?float
+    {
+        return $this->sousTotal;
+    }
+
+    public function setSousTotal(float $sousTotal): static
+    {
+        $this->sousTotal = $sousTotal;
+
+        return $this;
+    }
+
+   
+    public function getStatut(): ?StatutCommande
+    {
+        return $this->statut;
+    }
+
+    public function setStatut(StatutCommande $statut): static
+    {
+        $this->statut = $statut;
+
+        return $this;
+    }
+}
