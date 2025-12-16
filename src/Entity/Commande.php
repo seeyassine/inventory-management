7bbd0e5bb786ed339afa3bc51f8e17d05b36d83d
See Yassine <seeyassine@gmail.com>
2025-12-16 21:53:26 +0100

    add Commande Entity

---
+++ src/Entity/Commande.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Entity;
+
+use App\Enum\StatutCommande;
+use App\Repository\CommandeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CommandeRepository::class)]
+class Commande
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $numero = null;
+
+    #[ORM\Column]
+    private ?\DateTime $dateCommande = null;
+
+    #[ORM\Column(enumType: StatutCommande::class)]
+    private ?StatutCommande $statut;
+
+    public function __construct()
+    {
+        $this->statut = StatutCommande::BROUILLON;
+    }
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNumero(): ?string
+    {
+        return $this->numero;
+    }
+
+    public function setNumero(string $numero): static
+    {
+        $this->numero = $numero;
+
+        return $this;
+    }
+
+    public function getDateCommande(): ?\DateTime
+    {
+        return $this->dateCommande;
+    }
+
+    public function setDateCommande(\DateTime $dateCommande): static
+    {
+        $this->dateCommande = $dateCommande;
+
+        return $this;
+    }
+
+    public function getStatut(): ?StatutCommande
+    {
+        return $this->statut;
+    }
+
+    public function setStatut(StatutCommande $statut): static
+    {
+        $this->statut = $statut;
+
+        return $this;
+    }
+}
